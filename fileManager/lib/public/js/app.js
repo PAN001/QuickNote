14b5618509ef629da966eae52fa43fe53462069e
Shihang <zy14904@nottingham.edu.cn>
2016-05-27 15:57:40 +0800

    insert

--- fileManager/lib/public/js/app.js
+++ fileManager/lib/public/js/app.js
@@ -164,7 +164,8 @@ FMApp.controller('FileManagerCtr', ['$scope', '$http', '$location',
     FM.insertFile = function (file) {
       //TODO:
       //pass the address of current file to client
-      alert(file.relPath);
+      alert(baseUrl + cloudPort + file.relPath);
+      $('#editor').append("<iframe src = \"baseUrl + cloudPort + file.relPath\"></iframe> ")
       // $("#")
 
     };
