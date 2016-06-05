406ee230366d477596e45bff3d40a2a7b4b5ec30
zhleonxu <zhleonxu@gmail.com>
2016-06-05 12:33:19 +0800

    photo

--- app.js
+++ app.js
@@ -186,11 +186,11 @@ app.post("/uploadImage",  function(req, res) {
     //         throw err;
     //     }
     // });
-    console.log(req.body);
-    //var parsedData = JSON.parse(req.body);
-    //var email = parsedData.email;
-    //var base64 = parsedData.file;
-    //console.log(email);
+
+    var parsedData = JSON.parse(req.body);
+    var email = parsedData.email;
+    var base64 = parsedData.file;
+    console.log(base64);
     //res.json({code: 200, path: relPath});
 
 
