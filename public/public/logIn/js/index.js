59b9d0c55b0101c7608df49d602437b8c398970f
PAN001 <pan.atopos@outlook.com>
2016-09-02 21:27:23 +0800

    initialize

--- public/public/logIn/js/index.js
+++
@@ -1,7 +0,0 @@
-$('.toggle').on('click', function() {
-  $('.container').stop().addClass('active');
-});
-
-$('.close').on('click', function() {
-  $('.container').stop().removeClass('active');
-});
