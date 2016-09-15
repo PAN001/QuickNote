6467a9a21a3af167be8e88821b814f7a278d687b
PAN001 <pan.atopos@outlook.com>
2016-09-15 17:32:46 +0800

    add js-based building

---
+++ build.js
@@ -0,0 +1,50 @@
+var NwBuilder = require('nw-builder');
+// var gulp = require('gulp');
+// var gutil = require('gulp-util');
+
+// gulp.task('nw', function () {
+
+//     var nw = new NwBuilder({
+//         // version: '0.14.6',
+//         files: './public/*',
+//         macIcns: './icons/app.icns',
+//         // macPlist: {mac_bundle_id: 'myPkg'},
+//         // platforms: ['win32', 'win64', 'osx64']
+//         platforms: ['osx64'],
+//         buildDir: './build'
+//     });
+
+//     // Log stuff you want
+//     nw.on('log', function (msg) {
+//         gutil.log('nw-builder', msg);
+//     });
+
+//     // Build returns a promise, return it so the task isn't called in parallel
+//     return nw.build().catch(function (err) {
+//         console.log(err);
+//         gutil.log('nw-builder', err);
+//     });
+// });
+
+// gulp.task('default', ['nw']);
+
+
+var nw = new NwBuilder({
+    // version: '0.14.6',
+    files: './public/**',
+    macIcns: './icons/app.icns',
+    // macPlist: {mac_bundle_id: 'myPkg'},
+    // platforms: ['win32', 'win64', 'osx64']
+    platforms: ['osx64'],
+    buildDir: './build'
+});
+
+//Log stuff you want
+nw.on('log',  console.log);
+
+// Build returns a promise
+nw.build().then(function () {
+   console.log('all done!');
+}).catch(function (error) {
+    console.error(error);
+});
