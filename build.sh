57b448ef1faa7ebbd95e281945fe71c97d8d4e05
tiz170 <jiaojiao1210@gmail.com>
2021-09-06 15:42:23 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 # build the nwjs application
 ./node_modules/nw-builder/bin/nwbuild -p osx64 -o ./build ./public
+./node_modules/nw-builder/bin/nwbuild -p win64 -o ./build ./public
+./node_modules/nw-builder/bin/nwbuild -p linux64 -o ./build ./public
